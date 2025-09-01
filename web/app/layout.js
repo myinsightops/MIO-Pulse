44a78d5df8c28df7da976225b869964a735c8911
myinsightops <myinsightops@outlook.com>
2025-09-02 00:28:27 +0200

    chore: switch to next/font for Wix Madefor Text

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,6 +1,12 @@
 import "./globals.css"
 import Banner from "@/components/Banner/Banner"
 import Nav from "@/components/Nav/Nav"
+import { Wix_Madefor_Text } from "next/font/google"
+
+const wix = Wix_Madefor_Text({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+})
 
 export const metadata = {
   title: "MyInsightOps",
@@ -10,16 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Fonts: Wix Madefor Text */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Wix+Madefor+Text:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-sans antialiased">
+      <body className={`${wix.className} font-sans antialiased`}>
         <Nav />
         {children}
         <Banner />
